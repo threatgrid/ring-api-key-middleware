b3c8160a5529c2b3849c6ce22bc659a8989f5c5b
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2017-11-03 16:02:29 +0100

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/ring-api-key-middleware "0.1.0"
+(defproject threatgrid/ring-api-key-middleware "0.1.1-SNAPSHOT"
   :description "A simple middleware to deal with API keys Authentication"
   :url "http://github.com/threatgrid/ring-api-key-middleware"
   :license {:name "Eclipse Public License - v 1.0"
